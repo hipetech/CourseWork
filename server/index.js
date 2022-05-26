4b8083dc71b95a61d5f51d646bd364c871df21ee
hipetech <64170764+hipetech@users.noreply.github.com>
2022-05-26 09:58:12 +0300

    post route has been added

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const ClientConnection = require("./database/ClientConnection");
+const {json} = require("express");
+const {stringify} = require("nodemon/lib/utils");
+const bodyParser = require("body-parser");
+
+
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 const PORT = 5000;
 
 app.listen(PORT, () => console.log(`Server has been started on port ${PORT}`));
@@ -24,5 +30,10 @@ app.get('/getService', async (req, res) => {
     const result = await client.selectService()
     res.send(result);
     client.disableConnection();
+});
 
+app.post('/postData', (req, res) => {
+    console.log(req.body);
+    res.sendStatus(200);
 })
+
